3ee8216e36f24ffb3557ebb367bb633fb271940a
Barnoch Anas <anasbarnoch@gmail.com>
2022-11-17 23:53:46 +0100

    second commit

--- starter/profile.php
+++ starter/profile.php
@@ -4,8 +4,30 @@
     include_once 'sidebar.php' ;
 ?>
 
+    <form action="scripts.php" method="POST" enctype="multipart/form-data" class="updateForm">
+        <div class="mt-4 mx-5 text-center">
+            <img src="/design/img/form/utilisateur.png" width="150" height="150">
+        </div>
+        <div class="mb-3">
+            <label for="nameBook" class="form-label">Username</label>
+            <input type="text" class="form-control" id="nameBook" name="title" required>
+        </div>
 
+        <div class="mb-3">
+            <label for="author" class="form-label">Email</label>
+            <input type="Email" class="form-control" id="email" name="email" required>
+        </div>
 
+        <div class="mb-3">
+            <label for="password" class="form-label">Password</label>
+            <input type="password" class="form-control" id="password" name="password" required>
+        </div>
+        
+      <div class="">
+        <button type="submit" class="btn btn-info text-white" name="update">Update</button>
+      </div>
+      
+      </form>
 
 
 <?php
